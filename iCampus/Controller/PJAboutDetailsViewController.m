866f1cc6d8c810e81c7c1c4c0e5d8e8dc24ac4ca
pjpjpj <877302410@qq.com>
2017-04-30 20:14:59 +0800

    完善 关于（关于详情数据接入完成）

--- iCampus/Controller/PJAboutDetailsViewController.m
+++ iCampus/Controller/PJAboutDetailsViewController.m
@@ -26,6 +26,9 @@
 - (void)initView {
     self.title = _dataSource[@"aboutName"];
     self.view.backgroundColor = [UIColor whiteColor];
+    UIWebView * webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [webView loadHTMLString:_dataSource[@"aboutDetails"] baseURL:nil];
+    [self.view addSubview:webView];
 }
 
 @end
